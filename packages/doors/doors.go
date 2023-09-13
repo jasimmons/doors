7cec869ced39e0c0314907b3e025ce0dd10dede7
jasimmons <1382389+jasimmons@users.noreply.github.com>
2023-09-13 21:59:31 +0000

    refactor function

--- packages/doors/doors.go
+++ packages/doors/doors.go
@@ -1,81 +1,66 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
 	"math/rand"
+	"strings"
 	"time"
 )
 
 var (
-	allKeys = []int{1, 2, 3, 4, 5, 6}
-
-	ErrInvalidClass = errors.New("invalid class")
+	// keys represents all valid keys for any/all classes.
+	keys = []int{1, 2, 3, 4, 5, 6}
 )
 
+// Main is the entrypoint of the request.
 func Main(args map[string]interface{}) map[string]interface{} {
-	class := "feeder"
-	if c, ok := args["class"].(string); ok {
-		class = c
+  raw := args["class"]
+  n := ndoors(raw)
+  if n == 0 {
+    return map[string]interface{}{
+			"error": fmt.Sprintf("unknown class %v", raw),
+		}
+  }
+
+  return map[string]interface{}{
+		"body": map[string]interface{}{
+			"keys": randomize(n),
+		},
 	}
-
-	msg := make(map[string]interface{})
-
-	keys, err := keysForClass(class)
-	if err != nil {
-		msg["error"] = err.Error()
-	}
-
-	shuffled := randomize(keys...)
-
-	shuffledMap := map[string]interface{}{
-		"keys": shuffled,
-	}
-	shuffledJson, err := json.Marshal(shuffledMap)
-	if err != nil {
-		msg["error"] = err.Error()
-	}
-
-	msg["body"] = fmt.Sprintf("%v", string(shuffledJson))
-	return msg
 }
 
-func keysForClass(class string) ([]int, error) {
-	numDoors, err := numDoorsForClass(class)
-	if err != nil {
-		return nil, err
+// ndoors returns the number of doors necessary to open for the given
+// class `cl`. Return 0 if `cl` is unknown/invalid.
+func ndoors(cl any) int {
+	class, ok := cl.(string)
+	if !ok {
+		return 0
 	}
-
-	return allKeys[:numDoors], nil
-}
-
-func numDoorsForClass(class string) (int, error) {
-	switch class {
-	case "feeder",
-		"barbarian",
-		"fighter",
-		"guardian",
-		"magician",
-		"rogue":
-		return 2, nil
+	switch strings.ToLower(strings.TrimSpace(class)) {
+	case "fighter", "barbarian", "rogue", "magician", "guardian":
+		return 2
 	case "samurai", "paladin":
-		return 3, nil
+		return 3
 	case "monk", "ninja":
-		return 4, nil
+		return 4
 	case "warlock", "headhunter":
-		return 5, nil
+		return 5
 	case "alchemist":
-		return 6, nil
+		return 6
 	default:
-		return 0, ErrInvalidClass
+		return 0
 	}
 }
 
-func randomize(keys ...int) []int {
+// randomize takes a number of 'keys' n and returns a slice with
+// n elements of randomized 'keys'.
+func randomize(n int) []int {
+  // Get a slice with n total keys
+  ret := keys[:n]
+  // Randomize the slice and return it
 	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(keys), func(i, j int) {
-		keys[i], keys[j] = keys[j], keys[i]
+	rand.Shuffle(len(ret), func(i, j int) {
+		ret[i], ret[j] = ret[j], ret[i]
 	})
-	return keys
+	return ret
 }
